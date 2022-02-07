a3b9cbb257b3704933bee1635b6e92201438ecd4
Filip Průcha <prucha.fil@gmail.com>
2022-02-07 14:29:36 +0100

    Car

---
+++ Car.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ATNC;
+
+internal class Car {
+	public Car() { }
+
+	public enum LightState { Off, Day, Meeting }
+
+	public ushort Speed { get; set; }
+	public LightState Light { get; set; }
+	public string Destination { get; set; }
+
+}
