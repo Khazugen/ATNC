d2c65d1878fc34b516147bc1a8fd73375a48bef5
Filip Průcha <prucha.fil@gmail.com>
2022-02-09 22:25:48 +0100

    tunnel

--- Car.cs
+++ Car.cs
@@ -7,12 +7,33 @@ using System.Threading.Tasks;
 namespace ATNC;
 
 internal class Car {
-	public Car() { }
-
 	public enum LightState { Off, Day, Meeting, Fog }
+	public enum RoadType { Normal = 50, Bridge = 80, Tunnel = 30 }
 
+	public Car(CarGPX gpx) => _gpx = gpx;
+
+	private readonly CarGPX _gpx;
 	public ushort Speed { get; set; }
 	public LightState Light { get; set; }
 	public string Destination { get; set; }
 
+	public delegate void RoadTypeChanged(object sender, RoadTypeEventArgs e);
+	public event RoadTypeChanged RoadChanged;
+
+	public void Drive() => _gpx.Margin = new System.Windows.Thickness(
+			_gpx.Margin.Left + Speed / 10,
+			_gpx.Margin.Top,
+			_gpx.Margin.Right,
+			_gpx.Margin.Bottom
+		);
+
+	public class RoadTypeEventArgs : EventArgs {
+		public RoadTypeEventArgs(RoadType roadType, ushort recommendedSpeed) {
+			RoadType = roadType;
+			RecommendedSpeed = (ushort)roadType;
+		}
+
+		public RoadType RoadType { get; }
+		public ushort RecommendedSpeed { get; }
+	}
 }
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,7 +1,18 @@
 ﻿using System.Windows;
+using System.Windows.Threading;
 
 namespace ATNC;
 
 public partial class MainWindow : Window {
-	public MainWindow() => InitializeComponent();
+	private readonly Car _car;
+	private readonly DispatcherTimer _cartimer = new();
+
+	public MainWindow() {
+		InitializeComponent();
+		_car =  new Car(car);
+		_cartimer.Interval = new System.TimeSpan(0, 0, 0, 0, 10);
+		_cartimer.Start();
+		_car.Speed = 50;
+		_cartimer.Tick += (sender, e) => _car.Drive();
+	}
 }
